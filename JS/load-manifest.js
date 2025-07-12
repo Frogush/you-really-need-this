e92b99dbd9e2027223200a4772597e15431e0b8e
Frogush <aes-09k@mail.ru>
2025-07-12 11:40:50 +0300

    add favicon/manifest and color preferens for favicon/manifest

---
+++ JS/load-manifest.js
@@ -0,0 +1,11 @@
+(function () {
+  const isDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  const manifestPath = isDark
+    ? '/Images/site-dark.webmanifest'
+    : '/Images/site-light.webmanifest';
+
+  const link = document.createElement('link');
+  link.rel = 'manifest';
+  link.href = manifestPath;
+  document.head.appendChild(link);
+})();
